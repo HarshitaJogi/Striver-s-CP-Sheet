757d4a78e511fad0907737f48dfae40a866c6d46
HarshitaJogi <109593461+HarshitaJogi@users.noreply.github.com>
2023-01-13 23:16:16 +0530

    Add files via upload

---
+++ Implementation/DreamoonAndStairs.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class DreamoonAndStairs{
+
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+	public static void main(String[] args) {
+		
+
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+		FastReader sc = new FastReader();
+		
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int lower = (n + 2 - 1)/2;
+        int upper = n;
+
+        boolean ispossible = false;
+        int i;
+        for(i=lower; i<=upper; i++){
+            if(i%m==0){
+                ispossible = true;
+                System.out.println(i);
+                break;
+            }
+        }
+        if(ispossible == false){
+            System.out.println(-1);
+        }
+
+
+	}
+}
