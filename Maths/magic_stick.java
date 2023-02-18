9e6f5b9a3e3fa11dd510b67f066cd033f2ce1cbc
HarshitaJogi <109593461+HarshitaJogi@users.noreply.github.com>
2023-02-18 13:50:10 +0530

    Add files via upload

---
+++ Maths/magic_stick.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class magic_stick{
+
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+	public static void main(String[] args) {
+		
+
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+		FastReader sc = new FastReader();
+		
+        int t = sc.nextInt();
+        for(int h=0; h<t; h++){
+
+            long x = sc.nextLong();
+            long y = sc.nextLong();
+
+            String ans = "NO";
+
+            if(x == y) ans = "YES";
+
+            else if(x>y && y>=1) ans = "YES";
+
+            else if(x==1) ans = "NO";
+
+            else if((x==2 || x==3) && y<4){
+                ans = "YES";
+            }
+
+            else if(x>3) ans = "YES";
+
+            System.out.println(ans);
+        }
+
+
+	}
+}
