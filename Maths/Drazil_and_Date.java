d92c3f9d6e7040b36b92d2e0af2aa91b5ff267db
HarshitaJogi <109593461+HarshitaJogi@users.noreply.github.com>
2023-02-18 14:52:40 +0530

    Add files via upload

---
+++ Maths/Drazil_and_Date.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Drazil_and_Date{
+
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+	public static void main(String[] args) {
+		
+
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+		FastReader sc = new FastReader();
+		
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long x = sc.nextLong();
+
+        if(a<0) a=-a;
+        if(b<0) b=-b;
+
+        long min = a+b;
+
+        String ans = "NO";
+
+        if(min == x) ans = "YES";
+        else if(x<min) ans = "NO";
+        else if((x-min)%2 == 0) ans = "YES";
+
+        System.out.println(ans); 
+
+	}
+}
