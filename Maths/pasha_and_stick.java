d5511e6be209e6ddb87f2844b45179cfc271bc00
HarshitaJogi <109593461+HarshitaJogi@users.noreply.github.com>
2023-02-18 11:50:30 +0530

    Add files via upload

---
+++ Maths/pasha_and_stick.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class pasha_and_stick{
+
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+	public static void main(String[] args) {
+		
+
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+		FastReader sc = new FastReader();
+		
+        long n = sc.nextInt();
+
+        if(n%2 != 0){
+            System.out.println(0);
+        }
+        else if(n%4 == 0){
+            System.out.println(n/4 - 1);
+        }
+        else{
+            System.out.println(n/4);
+        }
+
+
+	}
+}
