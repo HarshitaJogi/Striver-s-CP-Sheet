7f5d576bd5a04e8cea516ea0a9b89c2c2ca45bf8
HarshitaJogi <109593461+HarshitaJogi@users.noreply.github.com>
2023-02-19 17:38:24 +0530

    Create special_numbers.java

---
+++ Bit Manipulation/special_numbers.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class special_numbers{
+
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+	static int M = (int)1E9 + 7;
+
+    public static void main(String[] args) {
+		
+
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+		FastReader sc = new FastReader();
+		
+        int t = sc.nextInt();
+        for(int h=0; h<t; h++){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            long p = 1;
+            long ans = 0l;
+
+            for(int i=0; i<31; i++){
+                if((k & (1<<i))!=0){
+                    ans = (ans+p)%M;
+                }
+
+                p = (p*n)%M;
+            }
+
+            System.out.println(ans);
+
+        }
+
+
+	}
+}
